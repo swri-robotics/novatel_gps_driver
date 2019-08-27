b63ce739156eed62f49ca6827cd5ddc5eeb281e8
Michael McConnell <Michael.McConnell-2@leidos.com>
2019-08-27 10:36:49 -0400

    Updating checkout.sh script and CARMAUtils repo name

--- docker/checkout.sh
+++ docker/checkout.sh
@@ -18,5 +18,5 @@
 
 set -ex
 
-git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ~/src/CARMAMsgs
-git clone https://github.com/usdot-fhwa-stol/CARMADriverUtils.git ~/src/CARMADriverUtils
+git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ~/src/CARMAMsgs --branch Version_3.0.0
+git clone https://github.com/usdot-fhwa-stol/CARMAUtils.git ~/src/CARMAUtils --branch Version_3.0.0
