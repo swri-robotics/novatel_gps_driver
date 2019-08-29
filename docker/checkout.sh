95da5f4b5c1afaeb9454524995b44ff43c33000b
Michael McConnell <Michael.McConnell-2@leidos.com>
2019-08-29 14:21:52 -0400

    Add circle config to Novatel Driver

--- docker/checkout.sh
+++ docker/checkout.sh
@@ -18,5 +18,5 @@
 
 set -ex
 
-git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ~/src/CARMAMsgs --branch Version_3.0.0
-git clone https://github.com/usdot-fhwa-stol/CARMAUtils.git ~/src/CARMAUtils --branch Version_3.0.0
+git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ~/src/CARMAMsgs --branch Version_3.0.0 --depth 1
+git clone https://github.com/usdot-fhwa-stol/CARMAUtils.git ~/src/CARMAUtils --branch Version_3.0.0 --depth 1
