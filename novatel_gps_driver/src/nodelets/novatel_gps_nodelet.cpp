2d8177782fec0c98528ff900f052f8087c499cf8
P. J. Reed <preed@swri.org>
2020-04-27 10:44:19 -0500

    Missed a sleep
    Signed-off-by: P. J. Reed <preed@swri.org>

--- novatel_gps_driver/src/nodelets/novatel_gps_nodelet.cpp
+++ novatel_gps_driver/src/nodelets/novatel_gps_nodelet.cpp
@@ -530,7 +530,7 @@ namespace novatel_gps_driver
             // Spin once to let the ROS callbacks fire
             ros::spinOnce();
             // Sleep for a microsecond to prevent CPU hogging
-            boost::this_thread::sleep(boost::posix_time::microseconds(1));
+            rate.sleep()
           }  // While (gps_.IsConnected() && ros::ok()) (inner loop to process data from device)
         }
         else  // Could not connect to the device
