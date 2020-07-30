deff1ae7c03a5dc1d16a7ddfc29f3cd4755a050d
John Baird <57960283+jtbaird@users.noreply.github.com>
2020-07-30 13:48:44 -0400

    updated to use docker repo name (#56)

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -17,7 +17,7 @@
 USERNAME=usdotfhwastol
 
 cd "$(dirname "$0")"
-IMAGE=$(basename `git rev-parse --show-toplevel`)
+IMAGE=carma-novatel-gps-driver
 
 echo ""
 echo "##### $IMAGE Docker Image Build Script #####"
